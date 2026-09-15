78498b2cab858b333146d0a92564dcbbe2fe0716
Mungi Sathish <sathishmungi@gmail.com>
2026-09-15 15:20:34 +0530

    Create Process_of_all_num_in_one_inp.py

---
+++ Process_of_all_num_in_one_inp.py
@@ -0,0 +1,11 @@
+numbers = list(map(int, input().split()))
+
+for n in numbers:
+    if n == 2:
+        print(4)
+    elif n == 6:
+        print(9)
+    elif n == 8:
+        print(18)
+    elif n == 15:
+        print(243)
